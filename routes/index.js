870250b295916024751374de01651ee1b96c9a75
Shahrear002 <shabuz2900@gmail.com>
2018-05-27 03:20:18 +0600

    cart,checkout complete

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
@@ -8,6 +9,37 @@ router.get('/', function(req, res, next) {
 	Product.find(function(err, docs) {
 		res.render('index', { title: 'Shopping Cart', products: docs });
 	});
+}); 
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+	var productId = req.params.id;
+	var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+	Product.findById(productId, function(err, product) {
+		if (err) {
+			return res.redirect('/');
+		}
+		cart.add(product, product.id);
+		req.session.cart = cart;
+		console.log(req.session.cart);
+		res.redirect('/');
+	});
+});
+
+router.get('/shopping-cart',function(req, res, next) {
+	if(!req.session.cart) {
+		return res.render('shopping-cart', {title: 'Shopping Cart', products: null});
+	}
+	var cart = new Cart(req.session.cart);
+	res.render('shopping-cart', {title: 'Shopping Cart', products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/checkout', function(req, res, next) {
+	if(!req.session.cart) {
+		return res.redirect('shopping-cart');
+	}
+	var cart = new Cart(req.session.cart);
+	res.render('checkout', {title: 'Shopping Cart', total: cart.totalPrice});
 });
 
 module.exports = router;
